928f0d818f6b159473ef88697ceb480f1ad7d3cb
Adrian Marquis <adrian.marquis78@gmail.com>
2020-12-04 08:23:44 -0500

    countSheep complete

---
+++ sheep-jump.js
@@ -0,0 +1,10 @@
+function countSheep(num) {
+    if(num === 0) {
+        console.log(`All sheep jumped over the fence`);
+    } else {
+        console.log(`${num}: Another sheep jump over the fence`);
+        countSheep(num-1);
+    }
+}
+
+console.log(countSheep(3));
